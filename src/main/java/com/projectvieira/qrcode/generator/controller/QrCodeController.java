da5deb91e49c1acdcaac761dc542ed606318def5
paulovieira01 <paulodavidpd@hotmail.com>
2025-08-05 20:36:28 -0300

    "adapters3"

--- src/main/java/com/projectvieira/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/projectvieira/qrcode/generator/controller/QrCodeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  @RestController
 public class QrCodeController {
     @PostMapping
-    public ResponseEntity<QrCodeGenerateResponse> generate(RequestBody QrCodeGenerateRequest request){
+    public ResponseEntity<QrCodeGenerateResponse> generate(RequestBody QrCodeGenerateRequest ){
 
         return null;
     }
--- src/main/java/com/projectvieira/qrcode/generator/service/S3StorageAdapter.java
+++ src/main/java/com/projectvieira/qrcode/generator/service/S3StorageAdapter.java
@@ -1,10 +1,46 @@
 package com.projectvieira.qrcode.generator.service;
 
 import com.projectvieira.qrcode.generator.ports.StoragePort;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectAclRequest;
 
-public class S3StorageAdapter implements StoragePort{
+@Component
+public class S3StorageAdapter implements StoragePort {
+
     @Override
     public String uploadFile(byte[] fileData, String fileName, String contentType) {
-        return null;
-    }
-}
+
+        private final S3Client s3Client;
+        private final String bucketName;
+        private final String region;
+
+        public S3StorageAdapter(@Value("${aws.s3.region}") String region,
+                                @Value("${aws.s3.bucket-name}") String bucketName) {
+
+            this.bucketName = bucketName;
+            this.region = region;
+            this.s3Client = s3Client.builder()
+                .region(Region.of(this.region))
+                .build();
+
+
+            @Override
+        public String uploadFile(byte[] fileData, String fileName, String contentType) {
+        PutObjectAclRequest putObjectAclRequest = PutObjectAclRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(contentType)
+                .build();
+
+        s3Client.putObject(putObjectAclRequest, RequestBody.fromBytes(fileData));
+        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, fileName);
+            }
+        }
+
+
+
+
